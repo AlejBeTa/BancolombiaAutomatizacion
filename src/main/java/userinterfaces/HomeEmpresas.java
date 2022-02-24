06866afbd46d8b26420f65e162cd444e283022e8
unknown <albedoyat@unal.edu.co>
2022-02-23 19:09:14 -0500

    Se accede a la pagina de actualidad economica

--- src/main/java/userinterfaces/HomeEmpresas.java
+++ src/main/java/userinterfaces/HomeEmpresas.java
@@ -2,7 +2,7 @@ package userinterfaces;
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
-public class HomeEmpresas {
-    public static final Target CAPITAL_INTELIGENTE = Target.the("Capital inteligente").located(By.xpath("//*[@classname='menu-inferior']/nav/ul/li[5]/a"));
+public class HomeEmpresas {//*[@id="menu-sup"]/li[5]/a
+    public static final Target CAPITAL_INTELIGENTE = Target.the("Capital inteligente").located(By.linkText("Capital Inteligente"));
 
 }
