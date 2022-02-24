318cf4d02bda348420470b28dc5c8f8ffb65728e
unknown <albedoyat@unal.edu.co>
2022-02-24 09:51:20 -0500

    Se descarga el pdf, se lee y se verifica

--- src/main/java/utils/constants/Constants.java
+++ src/main/java/utils/constants/Constants.java
@@ -9,4 +9,10 @@ public class Constants {
     public static final int CERO=0;
     public static final int NUEVE=9;
     public static final String NOMBRE_REPORTE="OPEP+ aumentará la oferta de petróleo en julio de 2021";
+    public static final String RUTA = System.getProperty("user.dir");
+    public static final String DESCARGA = RUTA + "\\descargas";
+    public static final String RUTA_PDF = "descargas/informe-sectorial-petroleo-junio-2021.pdf";
+    public static final String PAGINA_UNO_PDF="Junio, 2021\n" +
+            "Dirección de Investigaciones Económicas, Sectoriales y de Mercados\n" +
+            "Informe mensual de petróleo y gas";
 }
